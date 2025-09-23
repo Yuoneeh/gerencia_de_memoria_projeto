940d87b317a5e61dbc915a3785a3f31389dad2cf
Eduardo Braga <Dudufelipeeduard@gmail.com>
2025-09-23 10:52:14 -0300

    Base

---
+++ godot/projeto_gerencia_de_memoria_godot/scenes/objects/memory_slot/dados.gd
@@ -0,0 +1,18 @@
+extends TextureRect
+
+@export var slot_type: int = 0
+
+@export var SIZE  = 0:
+	set(value):
+		SIZE = value
+		%debug.text = str(SIZE)
+
+@onready var property: Dictionary = {"TEXTURE": texture,
+									"SIZE": SIZE,
+									"SLOT_TYPE": slot_type}:
+	set(value):
+		property = value
+		
+		texture = property["TEXTURE"]
+		SIZE = property["SIZE"]
+		slot_type = property["SLOT_TYPE"]
---
+++ godot/projeto_gerencia_de_memoria_godot/scenes/objects/memory_slot/slot.gd
@@ -0,0 +1,29 @@
+extends PanelContainer
+
+@onready var texture_rect = $TextureRect
+
+func _get_drag_data(at_position: Vector2) -> Variant:
+	
+	set_drag_preview(get_preview())
+	
+	return texture_rect
+	
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return data is TextureRect
+	
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	var temp = texture_rect.property
+	texture_rect.property = data.property
+	data.property = temp
+
+func get_preview():
+	var preview_texture = TextureRect.new()
+	
+	preview_texture.texture = texture_rect.texture
+	preview_texture.expand_mode = 1
+	preview_texture.size = Vector2(30,30)
+	
+	var preview = Control.new()
+	preview.add_child(preview_texture)
+	
+	return preview
